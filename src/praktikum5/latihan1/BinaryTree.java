bb2c28c300f601e4e02c15b9a5db440fed649f0f
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-03-14 08:47:00 +0700

    add: latihan praktikum 5

---
+++ src/praktikum5/latihan1/BinaryTree.java
@@ -0,0 +1,66 @@
+package praktikum5.latihan1;
+
+public class BinaryTree {
+
+    public Node root;
+
+    public void NewNode(int data) {
+        root = NewNode(root, new Node(data));
+    }
+
+    private Node NewNode(Node root, Node newData) {
+        if (root == null) {
+            root = newData;
+            return root;
+        }
+
+        if (newData.data < root.data) {
+            root.left = NewNode(root.left, newData);
+        } else {
+            root.right = NewNode(root.right, newData);
+        }
+
+        return root;
+    }
+
+    public void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.println(node.data + " ");
+            inOrder(node.right);
+        }
+    }
+
+    public void preOrder(Node node) {
+        if (node != null) {
+            System.out.println(node.data + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    public void postOrder(Node node) {
+        if (node != null) {
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.println(node.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.NewNode(20);
+        tree.NewNode(2);
+        tree.NewNode(25);
+        tree.NewNode(37);
+        tree.NewNode(16);
+
+        System.out.println("\nPre Order: ");
+        tree.preOrder(tree.root);
+        System.out.println("\nIn Order: ");
+        tree.inOrder(tree.root);
+        System.out.println("\nPost Order: ");
+        tree.postOrder(tree.root);
+    }
+}
---
+++ src/praktikum5/latihan1/Node.java
@@ -0,0 +1,12 @@
+package praktikum5.latihan1;
+
+public class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
