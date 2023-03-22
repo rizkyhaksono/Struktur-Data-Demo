6ba24db6aa6c8d8836f8e556d8eb339b3c30c32d
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-03-23 01:51:51 +0700

    add: kegiatan 1 python code

--- src/praktikum1/latihan1/KotaPy.py
+++ src/praktikum1/latihan1/KotaPy.py
@@ -1,2 +1,16 @@
 __author__      = "rizkyhaksono"
 __copyright__   = "Copyright 2023, Malang"
+
+class KotaPy:
+
+    def __init__(self):
+        self.namaKota = "Malang"
+        self.jumlahKota = 2
+
+    def main(self):
+        print("Nama Kota: " + self.namaKota)
+        print("Jumlah Kota: " + str(self.jumlahKota))
+
+if __name__ == "__main__":
+    kota = KotaPy()
+    kota.main()
